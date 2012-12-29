a767bbf8c9fb54306a83460e7d94b1d6e1985059
mfcalvanese <mfcalvanese@gmail.com>
2012-12-29 03:47:44 -0500

    added CREATE/UPDATE/GET services for nodes

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 var graphsvc = require('./');
+
+var options = {
+   "nodeTypes":
+   [
+      { "name": "user", "key": "fbid" },
+      { "name": "band", "key": "fbid" },
+      { "name": "song", "key": "scid" }
+   ]
+}
+
+var app = graphsvc("http://localhost:7474", options);
+app.listen(3000);
